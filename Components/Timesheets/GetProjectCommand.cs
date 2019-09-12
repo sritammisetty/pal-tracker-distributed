c085a042b891bf82a501a3664be18d5a49d14adf
Sri Venkatesh <Sri_Tammisetty@dell.com>
2019-09-12 14:50:35 +0530

    Circuit Breaker

---
+++ Components/Timesheets/GetProjectCommand.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Threading.Tasks;
+using Steeltoe.CircuitBreaker.Hystrix;
+
+namespace Timesheets
+{
+    public class GetProjectCommand : HystrixCommand<ProjectInfo>
+    {
+        private readonly Func<long, Task<ProjectInfo>> _getProjectFn;
+        private readonly Func<long, Task<ProjectInfo>> _getProjectFallbackFn;
+        private readonly long _projectId;
+        
+        public GetProjectCommand(
+            Func<long, Task<ProjectInfo>> getProjectFn,
+            Func<long, Task<ProjectInfo>> getProjectFallbackFn,
+            long projectId
+        ) : base(HystrixCommandGroupKeyDefault.AsKey("ProjectClientGroup"))
+        {
+            _getProjectFn = getProjectFn;
+            _getProjectFallbackFn = getProjectFallbackFn;
+            _projectId = projectId;
+        }
+
+        protected override async Task<ProjectInfo> RunAsync() => await _getProjectFn(_projectId);
+        protected override async Task<ProjectInfo> RunFallbackAsync() => await _getProjectFallbackFn(_projectId);
+    }
+}
